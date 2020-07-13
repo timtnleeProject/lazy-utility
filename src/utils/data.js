ae4502cc93114f357a74d33bf24afd5f0d0d5f1d
Timothy <timothy_lee@trendmicro.com>
2020-07-13 15:55:08 +0800

    fix: 🐛 combineTemplateString first empty

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,9 +1,15 @@
 export const combineTemplateString = (...args) => {
   const [strs, ...exp] = args;
   let str = '';
+  let first = true;
   for (let i = 0; i < strs.length; i++) {
     if (exp[i]) {
-      str += strs[i] + exp[i];
+      if (first) {
+        str += exp[i];
+        first = false;
+      } else {
+        str += strs[i] + exp[i];
+      };
     } else if (i === strs.length - 1) {
       str += strs[i];
     }
